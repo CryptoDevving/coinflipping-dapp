80c1cb3dbfb7f3615007e1a212df0c3d59f45527
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-01-31 16:02:01 +0900

    chore: add coinunknown images

--- client/src/images/index.js
+++ client/src/images/index.js
@@ -1,7 +1,9 @@
 import ethone from './ethone.jpeg';
 import coinh from './coin_h.png';
 import coint from './coin_t.png';
+import coinu from './coin_u.png';
 
 export const EthOne = ethone;
 export const CoinHeads = coinh;
 export const CoinTails = coint;
+export const CoinUnknown = coinu;
