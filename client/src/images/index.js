15b68f99179b8b105b9110ccdb6ab5edc0667f83
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-01-31 11:51:39 +0900

    chore: add ethereum images

--- client/src/images/index.js
+++ client/src/images/index.js
@@ -1,3 +1,7 @@
 import ethone from './ethone.jpeg';
+import coinh from './coin_h.png';
+import coint from './coin_t.png';
 
 export const EthOne = ethone;
+export const CoinHeads = coinh;
+export const CoinTails = coint;
